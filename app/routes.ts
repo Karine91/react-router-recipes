00afb675ad123c5991ba0b4dd6340352bb0a9fbe
Karine91 <26932594+Karine91@users.noreply.github.com>
2026-07-08 22:32:24 +0200

    adding db

--- app/routes.ts
+++ app/routes.ts
@@ -1,11 +1,14 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"), 
-    route("/discover", 'routes/discover.tsx'),
-    route("/app", 'routes/app.tsx'),
-    route('/settings', 'routes/settings.tsx', [
-        route('profile', 'routes/settings/profile.tsx'),
-        route('app', 'routes/settings/app.tsx'),
-    ]),
+  index("routes/home.tsx"),
+  route("/discover", "routes/discover.tsx"),
+  route("/app", "routes/app.tsx", [
+    index("routes/app/index.tsx"),
+    route("pantry", "routes/app/pantry.tsx"),
+  ]),
+  route("/settings", "routes/settings.tsx", [
+    route("profile", "routes/settings/profile.tsx"),
+    route("app", "routes/settings/app.tsx"),
+  ]),
 ] satisfies RouteConfig;
--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,10 +1,27 @@
+import { NavLink, Outlet } from "react-router";
+import clsx from "clsx";
+
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-      <p>Welcome to the App!</p>
+    <div className="flex flex-col h-full">
+      <h1 className="text-2xl front-bold my-4">App</h1>
+      <nav className="mt-2 pb-2 border-b-2 border-b-gray-300">
+        <NavLink
+          to="pantry"
+          className={({ isActive }) =>
+            clsx("hover:text-gray-500 pb-2.5 px-2 md:px-4", {
+              "border-b-2 border-b-primary": isActive,
+            })
+          }
+        >
+          Pantry
+        </NavLink>
+      </nav>
+      <div className="py-4">
+        <Outlet />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
